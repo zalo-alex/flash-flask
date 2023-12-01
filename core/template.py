23fbfdb4480ab7f350426bc2597e7a9295e4715a
Alex Zalo <zalo.alex56@gmail.com>
2023-12-01 17:22:57 +0100

    add context to templates

--- core/template.py
+++ core/template.py
@@ -3,9 +3,9 @@ import os
 
 from flask import render_template_string
 
-def send_template():
+def send_template(**kwargs):
     path = inspect.getouterframes(inspect.currentframe(), 1)[1].filename
     filename = os.path.basename(path)[:-3] + ".html"
     folder = os.path.dirname(os.path.abspath(path))
     
-    return render_template_string(open(os.path.join(folder, filename), encoding="utf8").read())
+    return render_template_string(open(os.path.join(folder, filename), encoding="utf8").read(), **kwargs)
