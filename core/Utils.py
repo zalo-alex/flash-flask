416ccfef6e0e49a5f0ed7e66b7f9f46649d74f69
Alex Zalo <zalo.alex56@gmail.com>
2023-12-14 09:31:58 +0100

    get_json_values

--- core/Utils.py
+++ core/Utils.py
@@ -7,3 +7,9 @@ class Utils:
             return request.environ['REMOTE_ADDR']
         else:
             return request.environ['HTTP_X_FORWARDED_FOR']
+        
+    def get_json_values(array: list):
+        values = [value for value in map(lambda key: request.json.get(key), array)]
+        if any([value == None for value in values]):
+            raise Exception("Invalid request data")
+        return values
