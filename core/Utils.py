48b653f260f2672dc87e57fbee009ba208896a80
Alex Zalo <zalo.alex56@gmail.com>
2023-12-14 10:43:32 +0100

    get_optional_json_values

--- core/Utils.py
+++ core/Utils.py
@@ -13,3 +13,13 @@ class Utils:
         if any([value == None for value in values]):
             raise Exception("Invalid request data")
         return values
+    
+    def get_optional_json_values(array: list):
+        values = [value for value in map(lambda key: request.json.get(key), array)]
+        not_none = []
+        [not_none.append(value) if value != None else None for value in values]                
+        
+        if not not_none:
+            raise Exception("Invalid request data")
+
+        return not_none
